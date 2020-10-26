09c95ddd91ed20fb70b11be4e4173206b535363b
JustinASmith <justinasmith662@gmail.com>
2020-10-26 16:42:25 -0500

    Revamp project to try to mitigate code signing issues.
    Fresh project works fine for code signing, so I am trying the same approach here, but adding back each thing later.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,18 +5,26 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:sp_flutter_app/main.dart';
-import 'package:sp_flutter_app/presentation/screens/wrapper.dart';
 
 void main() {
-  testWidgets('Home contains \'Hello, world!\' text.',
-      (WidgetTester tester) async {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(Wrapper());
+    await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
-    expect(find.text('Hello, world!'), findsOneWidget);
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    // Verify that our counter has incremented.
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
   });
 }
