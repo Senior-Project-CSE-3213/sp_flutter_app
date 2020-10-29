1a48cbd5826e87966df58fdd72f2d7cd17d7cbd5
JustinASmith <justinasmith662@gmail.com>
2020-10-29 15:37:45 -0500

    Fix screens.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:sp_flutter_app/screens/event_map.dart';
 import 'package:sp_flutter_app/screens/help_screen.dart';
 import 'package:sp_flutter_app/screens/profile_screen.dart';
+import 'package:sp_flutter_app/screens/settings_screen.dart';
 import 'package:sp_flutter_app/screens/wrapper.dart';
 import 'package:sp_flutter_app/services/auth.dart';
 import 'package:sp_flutter_app/shared/constants.dart';
-
-import 'file:///E:/SeniorProject/sp_flutter_app/lib/screens/event_map.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,7 +18,8 @@ void main() async {
       ],
       child: MaterialApp(initialRoute: eventMapScreenRoute, routes: {
         eventMapScreenRoute: (_) => Wrapper(child: EventMap()),
-        profileScreenRoute: (_) => Wrapper(child: ProfileScreen()),
-        helpScreenRoute: (_) => Wrapper(child: HelpScreen()),
+        profileScreenRoute: (_) => ProfileScreen(),
+        helpScreenRoute: (_) => HelpScreen(),
+        settingScreenRoute: (_) => SettingScreen(),
       })));
 }
