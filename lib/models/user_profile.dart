4e92f737298d1679c5ecbe2f41751516189804f6
JustinASmith <justinasmith662@gmail.com>
2020-11-03 18:46:56 -0600

    Update User model and add UserProfile model.
    This was a team effort through VS Code Live Share.

---
+++ lib/models/user_profile.dart
@@ -0,0 +1,77 @@
+class UserProfile {
+  String firstName;
+  String lastName;
+  String profPicture;
+  // for favoriteSports datatype might be another model or enum instead of String later
+  List<String> favoriteSports;
+  // stats might be another model since we might want many different attributes held on it
+  String stats;
+  // followers is a list of strings because it will be a list of "uid"
+  List<String> followers;
+  List<String> following;
+
+  UserProfile(
+      {this.firstName,
+      this.lastName,
+      this.profPicture,
+      this.favoriteSports,
+      this.stats,
+      this.followers,
+      this.following});
+
+  String getFirstName() {
+    return firstName;
+  }
+
+  setFirstName(final String name) {
+    this.firstName = name;
+  }
+
+  String getLastName() {
+    return lastName;
+  }
+
+  setLastName(final String name) {
+    this.lastName = name;
+  }
+
+  String getProfPicture() {
+    return profPicture;
+  }
+
+  setProfPicture(final String url) {
+    this.profPicture = url;
+  }
+
+  List<String> getFollowers() {
+    return followers;
+  }
+
+  setFollowers(final List<String> follows) {
+    this.followers = follows;
+  }
+
+  addFollower(final String uid) {
+    this.followers.add(uid);
+  }
+
+  removeFollower(final String uid) {
+    this.followers.remove(uid);
+  }
+
+  List<String> getFollowing() {
+    return following;
+  }
+
+  setFollowing(final List<String> followed) {
+    this.followers = followed;
+  }
+
+  List<String> getFavoriteSports() {
+    return favoriteSports;
+  }
+
+  setFavoriteSport(final List<String> favSports) {
+    this.favoriteSports = favSports;
+  }
+}
