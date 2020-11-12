69a6c809895e496a3557d6d31e4d8f0386bc7bd3
JustinASmith <justinasmith662@gmail.com>
2020-11-12 09:31:28 -0600

    Refactor AuthService

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -29,48 +29,29 @@ class AuthService {
 
   // sign in anonymously
   Future<User> signInAnon() async {
-    try {
-      fbAuth.UserCredential result = await _auth.signInAnonymously();
-      fbAuth.User user = result.user;
-      return _userFromFirebaseUser(user);
-    } catch (e) {
-      // TODO: handle different
-      throw throw ("Unable to sign in anonymously.");
-    }
+    fbAuth.UserCredential result = await _auth.signInAnonymously();
+    fbAuth.User user = result.user;
+    return _userFromFirebaseUser(user);
   }
 
   Future<User> signInWithEmailAndPassword(String email, String password) async {
-    try {
-      fbAuth.UserCredential result = await _auth.signInWithEmailAndPassword(
-          email: email, password: password);
-      fbAuth.User user = result.user;
-      return _userFromFirebaseUser(user);
-    } catch (e) {
-      // TODO: handle different
-      throw throw ("Unable to sign in with email and password.");
-    }
+    fbAuth.UserCredential result = await _auth.signInWithEmailAndPassword(
+        email: email, password: password);
+    fbAuth.User user = result.user;
+    return _userFromFirebaseUser(user);
   }
 
   // register with email/password
-  Future<User> registerWithEmailAndPassword(String email, String password) async {
-    try {
-      fbAuth.UserCredential result = await _auth.createUserWithEmailAndPassword(
-          email: email, password: password);
-      fbAuth.User user = result.user;
-      return _userFromFirebaseUser(user);
-    } catch (e) {
-      // TODO: handle different
-      throw throw ("Unable to register in with email and password.");
-    }
+  Future<User> registerWithEmailAndPassword(
+      String email, String password) async {
+    fbAuth.UserCredential result = await _auth.createUserWithEmailAndPassword(
+        email: email, password: password);
+    fbAuth.User user = result.user;
+    return _userFromFirebaseUser(user);
   }
 
   // sign out
   Future<void> signOut() async {
-    try {
-      return await _auth.signOut();
-    } catch (e) {
-      // TODO: handle different
-      throw throw ("Unable to sign out.");
-    }
+    await _auth.signOut();
   }
 }
