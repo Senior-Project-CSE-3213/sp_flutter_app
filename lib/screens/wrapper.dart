8a20f8d5fbe22b75cf98beca76462d3e6fa67018
JustinASmith <justinasmith662@gmail.com>
2020-11-12 09:34:05 -0600

    Update wrapper widget to use AnimatedSwitcher.
    This makes the sign out feature animate instead of immediately go to sign in screen.

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:sp_flutter_app/models/user.dart';
 import 'package:sp_flutter_app/screens/authenticate/authenticate.dart';
-import 'package:sp_flutter_app/services/auth.dart';
 import 'package:sp_flutter_app/shared/constants.dart';
 import 'package:sp_flutter_app/shared/widgets/main_drawer.dart';
 import 'package:sp_flutter_app/shared/widgets/notification_drawer.dart';
@@ -19,47 +18,59 @@ class Wrapper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // final auth = Provider.of<AuthService>(context);
-    final _userViewModel = UserViewModel();
+    final _userViewModel = Provider.of<UserViewModel>(context);
     // return either Home or Authenticate widget or loading
     return StreamBuilder<User>(
-        stream: _userViewModel.user,
+        stream: _userViewModel.userStream,
         builder: (context, snapshot) {
+          print(snapshot.connectionState);
           if (snapshot.connectionState == ConnectionState.active) {
             User user = snapshot.data;
             if (user == null) {
-              return Authenticate();
+              return AnimatedSwitcher(
+                duration: Duration(milliseconds: 500),
+                transitionBuilder: (Widget child, Animation<double> animation) {
+                  return ScaleTransition(child: child, scale: animation);
+                },
+                child: Authenticate(),
+              );
             }
-            return Scaffold(
-              backgroundColor: Colors.grey[350],
-              key: _scaffoldKey,
-              appBar: AppBar(
-                title: Text('Home'),
-                centerTitle: false,
-                backgroundColor: mainColor,
-                textTheme: TextTheme(
-                  headline5: TextStyle(color: Colors.white),
+            return AnimatedSwitcher(
+              duration: Duration(milliseconds: 500),
+              transitionBuilder: (Widget child, Animation<double> animation) {
+                return ScaleTransition(child: child, scale: animation);
+              },
+              child: Scaffold(
+                backgroundColor: Colors.grey[350],
+                key: _scaffoldKey,
+                appBar: AppBar(
+                  title: Text('Home'),
+                  centerTitle: false,
+                  backgroundColor: mainColor,
+                  textTheme: TextTheme(
+                    headline5: TextStyle(color: Colors.white),
+                  ),
+                  //elevation: 0.0,
+                  actions: <Widget>[
+                    IconButton(
+                      icon: Icon(Icons.chat),
+                      onPressed: () {
+                        print("Clicked chat");
+                      },
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.notifications),
+                      onPressed: () {
+                        _scaffoldKey.currentState.openEndDrawer();
+                      },
+                    ),
+                  ],
                 ),
-                //elevation: 0.0,
-                actions: <Widget>[
-                  IconButton(
-                    icon: Icon(Icons.chat),
-                    onPressed: () {
-                      print("Clicked chat");
-                    },
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.notifications),
-                    onPressed: () {
-                      _scaffoldKey.currentState.openEndDrawer();
-                    },
-                  ),
-                ],
+                drawer: MainDrawer(),
+                endDrawer: NotificationDrawer(),
+                body: child,
+                bottomNavigationBar: BottomAppBar(),
               ),
-              drawer: MainDrawer(),
-              endDrawer: NotificationDrawer(),
-              body: child,
-              bottomNavigationBar: BottomAppBar(),
             );
           } else {
             return Scaffold(
