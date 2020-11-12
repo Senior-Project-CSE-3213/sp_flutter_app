dc124b14ffbc8033c2583ce644fe9ac3d0d6a5ee
JustinASmith <justinasmith662@gmail.com>
2020-11-12 09:36:44 -0600

    Use ChangeNotifierProvider for UserViewModel in main.dart so we have access to User model everywhere.

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,28 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:sp_flutter_app/screens/event_map.dart';
 import 'package:sp_flutter_app/screens/help_screen.dart';
 import 'package:sp_flutter_app/screens/profile_screen.dart';
 import 'package:sp_flutter_app/screens/settings_screen.dart';
 import 'package:sp_flutter_app/screens/wrapper.dart';
 import 'package:sp_flutter_app/shared/constants.dart';
+import 'package:sp_flutter_app/viewmodels/user_viewmodel.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(
-    MaterialApp(initialRoute: eventMapScreenRoute, routes: {
-      eventMapScreenRoute: (_) => Wrapper(child: EventMap()),
-      profileScreenRoute: (_) => ProfileScreen(),
-      helpScreenRoute: (_) => HelpScreen(),
-      settingScreenRoute: (_) => SettingScreen(),
-    }),
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => UserViewModel()),
+      ],
+      child: MaterialApp(initialRoute: eventMapScreenRoute, routes: {
+        eventMapScreenRoute: (_) => Wrapper(child: EventMap()),
+        profileScreenRoute: (_) => ProfileScreen(),
+        helpScreenRoute: (_) => HelpScreen(),
+        settingScreenRoute: (_) => SettingScreen(),
+      }),
+    ),
   );
 }
