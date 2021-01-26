87592ee622facea753508a20edc5291ed1313640
NathanCheshire <nathan.vincent.2.718@gmail.com>
2021-01-26 17:44:59 -0600

    Sponsored Events ListView scroller done

--- lib/views/EventList/event_list_view.dart
+++ lib/views/EventList/event_list_view.dart
@@ -18,9 +18,8 @@ class _EventListViewState extends State<EventListView> {
           iconList: [
             Icons.add_circle,
             Icons.home,
-            Icons.find_in_page,
+            Icons.find_in_page, //this one is irrelevant
             Icons.person,
-            //todo find better images
           ],
           
           onChange: (val) {
@@ -35,13 +34,13 @@ class _EventListViewState extends State<EventListView> {
         body: Container (
           height: size.height,
           child: Column (
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisAlignment: MainAxisAlignment.start,
              children: <Widget>[
                //sponsored events row
                Row (
                   children: [
                     Container (
-                      padding: const EdgeInsets.only(left: 40.0, top: 20),
+                      padding: const EdgeInsets.only(left: 20.0, top: 20),
                       child: 
                       Text ( 
                           "Sponsored Events",
@@ -50,10 +49,35 @@ class _EventListViewState extends State<EventListView> {
                     )
                   ],
                ),
-               // card view row
+               // card view row 
                Row (
-                  //todo method for loading in json data and making cards
+                 children: [
+                   Padding(
+                    padding: const EdgeInsets.only(top: 10),
 
+                     child: Container (
+                       height: 120,
+                       width: size.width,
+
+                      //I'm going to look at this: 
+                      //https://www.youtube.com/watch?v=neXfa4J752A&ab_channel=CodeX
+                      // to make it have indicator dots and maybe stackable
+                       child: ListView (
+                         scrollDirection: Axis.horizontal,
+                         children: <Widget>[
+                            CreateSponsoredEventCard("Intramural Football",
+                            "Intramural Fields","Friday, 6:30PM"),
+                            CreateSponsoredEventCard("Dawgs After Dark",
+                            "The Hump","Thursday, 9:30PM"),
+                            CreateSponsoredEventCard("Cowbell yell",
+                            "Bettersworth","Sunday, 5:00PM"),
+                            CreateSponsoredEventCard("Ted X Talk",
+                            "The Hump","Tuesday, 12:30PM"),
+                         ],
+                       ) 
+                     ),
+                   )
+                 ],
                ),
                //latest evnents and filter button
                Row(
@@ -62,7 +86,7 @@ class _EventListViewState extends State<EventListView> {
                   Column (  
                     children: [
                       Container (
-                        padding: const EdgeInsets.only(left: 40.0, top: 20),
+                        padding: const EdgeInsets.only(left: 20.0),
                         child: 
                         Text ( 
                             "Latest Events",
@@ -111,6 +135,90 @@ class _EventListViewState extends State<EventListView> {
           )
         )
       )
+    );
+  }
+}
+
+class CreateSponsoredEventCard extends StatelessWidget {
+  String _eventTitle;
+  String _eventLocation;
+  String _eventTime;
+
+  CreateSponsoredEventCard(String title, String location, String time) {
+    _eventTitle = title;
+    _eventLocation = location;
+    _eventTime = time;
+  }
+
+  //todo getters and setters
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible (
+      child: Padding(
+        padding: const EdgeInsets.only(left: 10, right: 10),
+        child: Container (
+          height: 140,
+          width: 180,
+          padding: const EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+                Colors.blue.shade700, Colors.deepPurpleAccent],
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+            ),
+            borderRadius: BorderRadius.all(Radius.circular(24)),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.blue.shade700.withOpacity(0.4), blurRadius: 4, spreadRadius: 2
+              )
+            ]
+          ),
+          child: Column (
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                child: Container (
+                  child: Text(
+                    this._eventTitle, style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    )
+                  ),
+                ),
+              ),
+
+              Expanded(
+                child: Container (
+                  child: Text(
+                    this._eventLocation, style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    )
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 2,),
+
+              Expanded(
+                child: Container (
+                  child: Text(
+                    this._eventTime, style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    )
+                  ),
+                ),
+              ),
+            ],
+          )
+        ),
+      ),
     );
   }
 }
