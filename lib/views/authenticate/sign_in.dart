cd447643afdaab13668b0194ab9530b2964a74e3
Justin Smith <justinasmith662@gmail.com>
2021-02-07 22:53:37 -0600

    Adding more logic to sign in view

--- lib/views/authenticate/sign_in.dart
+++ lib/views/authenticate/sign_in.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:provider/provider.dart';
-import 'package:sp_flutter_app/views/views.dart';
-import 'package:sp_flutter_app/views/wrapper.dart';
+import 'package:sp_flutter_app/shared/widgets/full_width_text_btn_with_icon.dart';
+import 'package:sp_flutter_app/shared/widgets/heading_text.dart';
+import 'package:sp_flutter_app/shared/widgets/scaffold_with_gradient.dart';
+import 'package:sp_flutter_app/shared/widgets/simple_input_label.dart';
+import 'package:sp_flutter_app/shared/widgets/simple_text_input_field.dart';
+import 'package:sp_flutter_app/utils/helpers.dart';
 import '../../shared/constants.dart';
 import '../../shared/loading.dart';
 import '../../shared/response.dart';
@@ -14,83 +18,99 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
-  final _formKey = GlobalKey<FormState>();
-
   // Text field state
-  String email = '';
   String password = '';
+  String email = '';
+  bool emailError = false;
+  bool pwdError = false;
 
-  Widget _signInScreen(BuildContext context, UserViewModel _userViewModel) {
-    return Scaffold(
-      backgroundColor: Colors.grey[350],
-      appBar: AppBar(
-        backgroundColor: primaryColor,
-        textTheme: TextTheme(
-          headline6: TextStyle(color: Colors.white),
-        ),
-        elevation: 0.0,
-        title: Text('Sign in to App'),
-        actions: <Widget>[
-          FlatButton.icon(
-            icon: Icon(Icons.person),
-            label: Text('Register'),
-            textColor: Colors.white,
-            onPressed: () {
-              Navigator.of(context).pushNamed(registerViewRoute);
-            },
-          )
-        ],
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            //mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              SizedBox(height: 20.0),
-              TextFormField(
-                decoration:
-                    textInputDecoration.copyWith(hintText: 'Enter email'),
-                validator: (val) => val.isEmpty ? 'Enter an email' : null,
-                onChanged: (val) {
-                  setState(() => email = val);
-                },
-              ),
-              SizedBox(height: 20.0),
-              TextFormField(
-                decoration:
-                    textInputDecoration.copyWith(hintText: 'Enter password'),
-                obscureText: true,
-                validator: (val) => val.length < 6
-                    ? 'Password must be at least 6 chars long'
-                    : null,
-                onChanged: (val) {
-                  setState(() => password = val);
-                },
-              ),
-              SizedBox(height: 20.0),
-              RaisedButton(
-                color: Colors.deepOrange[400],
-                child: Text(
-                  'Sign In',
-                  style: TextStyle(color: Colors.white),
+  bool handleSubmit() {
+    setState(() {
+      emailError = false;
+      pwdError = false;
+    });
+    if (email.isEmpty) {
+      setState(() {
+        emailError = true;
+      });
+      return false;
+    }
+    if (!isPasswordCompliant(password)) {
+      setState(() {
+        pwdError = true;
+      });
+      return false;
+    }
+    return true;
+  }
+
+  void handlePasswordChange(String pwd) {
+    setState(() {
+      password = pwd;
+    });
+  }
+
+  Widget _registerScreen(BuildContext context, UserViewModel _userViewModel) {
+    final UserViewModel _userViewModel = Provider.of<UserViewModel>(context);
+    Size size = MediaQuery.of(context).size;
+    return ScaffoldWithGradient(
+      children: [
+        SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.symmetric(
+              horizontal: kDefaultPadding * 2.0,
+              vertical: size.height * 0.15,
+            ),
+            color: Colors.transparent,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                HeadingText(text: "Sign In"),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: kDefaultPadding * 2.0,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SimpleInputLabel(text: "Email"),
+                      SimpleTextInputField(
+                        error: emailError,
+                        handleSubmit: handleSubmit,
+                        handleChange: (val) {
+                          setState(() {
+                            email = val;
+                          });
+                        },
+                      ),
+                      SimpleInputLabel(text: "Password"),
+                      SimpleTextInputField(
+                        error: pwdError,
+                        obscure: true,
+                        handleSubmit: handleSubmit,
+                        handleChange: (pwd) => handlePasswordChange(pwd),
+                      ),
+                      SizedBox(height: kDefaultPadding * 2.0),
+                      FullWidthTextButtonWithIcon(
+                        handleSubmit: ({success}) async {
+                          if (handleSubmit()) {
+                            await _userViewModel.signInWithEmailAndPassword(
+                                email, password);
+                          }
+                        },
+                        svgAsset: "assets/svgs/envelop.svg",
+                        text: "Sign In with Email",
+                      ),
+                    ],
+                  ),
                 ),
-                onPressed: () async {
-                  if (_formKey.currentState.validate()) {
-                    await _userViewModel.signInWithEmailAndPassword(
-                        email, password);
-                    if (_userViewModel?.viewModelResponse?.exception == null) {
-                      Navigator.of(context)
-                          .restorablePushReplacementNamed(mapViewRoute);
-                    }
-                  }
-                },
-              ),
-            ],
+              ],
+            ),
           ),
         ),
-      ),
+      ],
     );
   }
 
@@ -114,11 +134,11 @@ class _SignInState extends State<SignIn> {
           );
           _userViewModel.viewModelResponse.showToast = false;
         }
-        return _signInScreen(context, _userViewModel);
+        return _registerScreen(context, _userViewModel);
         break;
       case ResponseState.COMPLETE:
       default:
-        return _signInScreen(context, _userViewModel);
+        return _registerScreen(context, _userViewModel);
         break;
     }
   }
