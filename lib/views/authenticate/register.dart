532903244018fa9a9660d3fda43ba358d58bd09a
Justin Smith <justinasmith662@gmail.com>
2021-02-07 22:53:24 -0600

    Adding more logic to register view

--- lib/views/authenticate/register.dart
+++ lib/views/authenticate/register.dart
@@ -1,12 +1,14 @@
-import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:profanity_filter/profanity_filter.dart';
 import 'package:provider/provider.dart';
 import 'package:sp_flutter_app/shared/widgets/full_width_text_btn_with_icon.dart';
 import 'package:sp_flutter_app/shared/widgets/heading_text.dart';
 import 'package:sp_flutter_app/shared/widgets/scaffold_with_gradient.dart';
 import 'package:sp_flutter_app/shared/widgets/simple_input_label.dart';
 import 'package:sp_flutter_app/shared/widgets/simple_text_input_field.dart';
+import 'package:sp_flutter_app/utils/helpers.dart';
+import 'package:sp_flutter_app/viewmodels/user_profile_viewmodel.dart';
 import '../../shared/constants.dart';
 import '../../shared/loading.dart';
 import '../../shared/response.dart';
@@ -25,54 +27,29 @@ class _RegisterState extends State<Register> {
   bool preferredNameError = false;
   bool pwd1Error = false;
   bool pwd2Error = false;
+  final profanityFilter = ProfanityFilter();
 
-  /*
-      Password Must Contain:
-      - Minimum 1 Upper case
-      - Minimum 1 lowercase
-      - Minimum 1 Numeric Number
-      - Minimum 1 Special Character
-      - Common Allow Character ( ! @ # $ & * ~ )
-    */
-  bool isPasswordCompliant(String password, [int minLength = 6]) {
-    if (password == null || password.isEmpty) {
-      return false;
-    }
-
-    bool hasUppercase = password.contains(new RegExp(r'[A-Z]'));
-    bool hasDigits = password.contains(new RegExp(r'[0-9]'));
-    bool hasLowercase = password.contains(new RegExp(r'[a-z]'));
-    bool hasSpecialCharacters =
-        password.contains(new RegExp(r'[!@#$%^&*(),.?":{}|<>]'));
-    bool hasMinLength = password.length > minLength;
-
-    return hasDigits &
-        hasUppercase &
-        hasLowercase &
-        hasSpecialCharacters &
-        hasMinLength;
-  }
-
-  void handleSubmit() {
+  bool handleSubmit() {
     setState(() {
       preferredNameError = false;
       pwd1Error = false;
       pwd2Error = false;
     });
-    if (preferredName.isEmpty) {
+    // not a perfect profanity filter
+    if (preferredName.isEmpty || profanityFilter.hasProfanity(preferredName)) {
       setState(() {
         preferredNameError = true;
       });
-      return;
+      return false;
     }
     if (password1 != password2 || !isPasswordCompliant(password1)) {
       setState(() {
         pwd1Error = true;
         pwd2Error = true;
       });
-      return;
+      return false;
     }
-    Navigator.of(context).pushNamed(registerViewRoute);
+    return true;
   }
 
   void handlePasswordChange(String pwd, bool pwd1) {
@@ -87,103 +64,130 @@ class _RegisterState extends State<Register> {
     }
   }
 
-  Widget _registerScreen(BuildContext context, UserViewModel _userViewModel) {
+  Widget _registerScreen(BuildContext context, UserViewModel _userViewModel,
+      UserProfileViewModel _userProfileViewModel) {
     Size size = MediaQuery.of(context).size;
     return ScaffoldWithGradient(
       children: [
-        Container(
-          margin: EdgeInsets.symmetric(
-            horizontal: kDefaultPadding * 2.0,
-            vertical: size.height * 0.15,
-          ),
-          color: Colors.transparent,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              HeadingText(text: "Register"),
-              Padding(
-                padding: const EdgeInsets.only(
-                  top: kDefaultPadding,
-                  bottom: kDefaultPadding,
-                ),
-                child: RichText(
-                  text: TextSpan(
-                    text: 'Using',
-                    style: TextStyle(
-                      color: Color(0xffadadb2),
-                      fontSize: 14,
-                      fontFamily: "Inter",
-                      fontWeight: FontWeight.w700,
-                      letterSpacing: 0.50,
-                    ),
-                    children: [
-                      TextSpan(
-                        text: ' test@domain.coms ',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 14,
-                          fontFamily: "Inter",
-                          fontWeight: FontWeight.w700,
-                          letterSpacing: 0.015,
-                        ),
+        SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.symmetric(
+              horizontal: kDefaultPadding * 2.0,
+              vertical: size.height * 0.15,
+            ),
+            color: Colors.transparent,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                HeadingText(text: "Register"),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    top: kDefaultPadding,
+                    bottom: kDefaultPadding,
+                  ),
+                  child: RichText(
+                    text: TextSpan(
+                      text: 'Using',
+                      style: TextStyle(
+                        color: Color(0xffadadb2),
+                        fontSize: 14,
+                        fontFamily: "Inter",
+                        fontWeight: FontWeight.w700,
+                        letterSpacing: 0.50,
                       ),
-                      TextSpan(
-                        text: 'to register',
-                        style: TextStyle(
-                          color: Color(0xffadadb2),
-                          fontSize: 14,
-                          fontFamily: "Inter",
-                          fontWeight: FontWeight.w700,
-                          letterSpacing: 0.50,
+                      children: [
+                        TextSpan(
+                          text: ' ${_userViewModel.registrationEmail} ',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14,
+                            fontFamily: "Inter",
+                            fontWeight: FontWeight.w700,
+                            letterSpacing: 0.015,
+                          ),
                         ),
+                        TextSpan(
+                          text: 'to register',
+                          style: TextStyle(
+                            color: Color(0xffadadb2),
+                            fontSize: 14,
+                            fontFamily: "Inter",
+                            fontWeight: FontWeight.w700,
+                            letterSpacing: 0.50,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: kDefaultPadding * 2.0,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SimpleInputLabel(text: "Preferred Name"),
+                      SimpleTextInputField(
+                        error: preferredNameError,
+                        handleSubmit: handleSubmit,
+                        handleChange: (val) {
+                          setState(() {
+                            preferredName = val;
+                          });
+                        },
+                      ),
+                      SimpleInputLabel(text: "Password"),
+                      SimpleTextInputField(
+                        error: pwd1Error,
+                        obscure: true,
+                        handleSubmit: handleSubmit,
+                        handleChange: (pwd) => handlePasswordChange(pwd, true),
+                      ),
+                      SimpleInputLabel(text: "Confirm Password"),
+                      SimpleTextInputField(
+                        error: pwd2Error,
+                        obscure: true,
+                        handleSubmit: handleSubmit,
+                        handleChange: (pwd) => handlePasswordChange(pwd, false),
+                      ),
+                      SizedBox(height: kDefaultPadding * 2.0),
+                      FullWidthTextButtonWithIcon(
+                        handleSubmit: ({success}) async {
+                          if (handleSubmit()) {
+                            // set the user profile preferred name
+                            _userProfileViewModel.userProfile
+                                .setPreferredName(preferredName);
+                            // register user (This does NOT create profile.)
+                            await _userViewModel.registerWithEmailAndPassword(
+                                _userViewModel.registrationEmail, password1);
+                            if (_userViewModel.viewModelResponse.state ==
+                                ResponseState.ERROR) {
+                              print("ERROR: There was an error registering.");
+                            } else {
+                              // Start setting data for the profile
+                              // Again (This does NOT create the profile either.)
+                              // When we actually call the method to add the user
+                              // profile to the DB is when the profile is actually created
+                              _userProfileViewModel.userProfile
+                                  .setUid(_userViewModel.user.uid);
+                              _userProfileViewModel.userProfile
+                                  .setPreferredName(preferredName);
+                              Navigator.of(context)
+                                  .pushNamed(createProfileViewRoute);
+                            }
+                          }
+                        },
+                        svgAsset: "assets/svgs/envelop.svg",
+                        text: "Register with Email",
                       ),
                     ],
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  vertical: kDefaultPadding * 2.0,
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SimpleInputLabel(text: "Preferred Name"),
-                    SimpleTextInputField(
-                      error: preferredNameError,
-                      handleSubmit: handleSubmit,
-                      handleChange: (val) {
-                        setState(() {
-                          preferredName = val;
-                        });
-                      },
-                    ),
-                    SimpleInputLabel(text: "Password"),
-                    SimpleTextInputField(
-                      error: pwd1Error,
-                      obscure: true,
-                      handleSubmit: handleSubmit,
-                      handleChange: (pwd) => handlePasswordChange(pwd, true),
-                    ),
-                    SimpleInputLabel(text: "Confirm Password"),
-                    SimpleTextInputField(
-                      error: pwd2Error,
-                      obscure: true,
-                      handleSubmit: handleSubmit,
-                      handleChange: (pwd) => handlePasswordChange(pwd, false),
-                    ),
-                    SizedBox(height: kDefaultPadding * 2.0),
-                    FullWidthTextButtonWithIcon(
-                      handleSubmit: handleSubmit,
-                      svgAsset: "assets/svgs/envelop.svg",
-                      text: "Register with Email",
-                    ),
-                  ],
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ],
@@ -193,6 +197,7 @@ class _RegisterState extends State<Register> {
   @override
   Widget build(BuildContext context) {
     final _userViewModel = Provider.of<UserViewModel>(context);
+    final _userProfileViewModel = Provider.of<UserProfileViewModel>(context);
     switch (_userViewModel.viewModelResponse.state) {
       case ResponseState.LOADING:
         return Loading();
@@ -210,11 +215,11 @@ class _RegisterState extends State<Register> {
           );
           _userViewModel.viewModelResponse.showToast = false;
         }
-        return _registerScreen(context, _userViewModel);
+        return _registerScreen(context, _userViewModel, _userProfileViewModel);
         break;
       case ResponseState.COMPLETE:
       default:
-        return _registerScreen(context, _userViewModel);
+        return _registerScreen(context, _userViewModel, _userProfileViewModel);
         break;
     }
   }
