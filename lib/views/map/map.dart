9bab4e05643382a165494fb98366a7f9051d3385
Justin Smith <justinasmith662@gmail.com>
2021-03-11 12:39:06 -0600

    Update map view to have overflowing container

--- lib/views/map/map.dart
+++ lib/views/map/map.dart
@@ -1,36 +1,19 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:sp_flutter_app/shared/widgets/heading_text.dart';
 import 'package:sp_flutter_app/shared/widgets/scaffold_with_gradient.dart';
+import 'package:sp_flutter_app/shared/widgets/subheading_text.dart';
 import '../../shared/constants.dart';
 
 class MapState extends State<MapView> with SingleTickerProviderStateMixin {
-  AnimationController animationController;
-  Animation degOneTranslationAnimation;
-  Animation rotationAnimation;
-
-  double getRadiansFromDegree(double degree) {
-    double unitRadian = 57.295779513;
-    return degree / unitRadian;
-  }
-
-  initState() {
-    animationController =
-        AnimationController(vsync: this, duration: Duration(milliseconds: 250));
-    degOneTranslationAnimation =
-        Tween(begin: 0.0, end: 1.0).animate(animationController);
-    rotationAnimation = Tween(begin: 180.0, end: 0.0).animate(
-        CurvedAnimation(parent: animationController, curve: Curves.easeOut));
-    super.initState();
-    animationController.addListener(() {
-      setState(() {});
-    });
-  }
-
   build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return ScaffoldWithGradient(
-      children: <Widget>[
+      menuColor: primaryColor,
+      children: [
         Container(
+          height: size.height * 0.75,
           child: GoogleMap(
             myLocationButtonEnabled: false,
             zoomControlsEnabled: false,
@@ -40,142 +23,103 @@ class MapState extends State<MapView> with SingleTickerProviderStateMixin {
             ),
           ),
         ),
-        Container(
-          //width: 90,
-          //height: 90,
-          child: Stack(
-            children: <Widget>[
-              Positioned(
-                  right: 30,
-                  bottom: 30,
-                  child: Stack(
-                    alignment: Alignment.bottomRight,
-                    children: <Widget>[
-                      IgnorePointer(
-                        child: Container(
-                          color: Colors.black.withOpacity(
-                              0.0), // comment or change to transparent color
-                          height: 150.0,
-                          width: 150.0,
-                        ),
-                      ),
-                      Transform.translate(
-                        offset: Offset.fromDirection(getRadiansFromDegree(270),
-                            degOneTranslationAnimation.value * 100),
-                        child: Transform(
-                          transform: Matrix4.rotationZ(
-                              getRadiansFromDegree(rotationAnimation.value))
-                            ..scale(degOneTranslationAnimation.value),
-                          alignment: Alignment.center,
-                          child: CircularButton(
-                            color: Colors.blue,
-                            width: 50,
-                            height: 50,
-                            icon: Icon(
-                              Icons.location_searching,
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            width: size.width,
+            height: size.height * 0.35,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30),
+                topRight: Radius.circular(30),
+              ),
+              color: primaryColor,
+              boxShadow: [
+                BoxShadow(color: primaryColor, spreadRadius: 1, blurRadius: 16),
+              ],
+            ),
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(kDefaultPadding * 2.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    HeadingText(
+                      text: "View Latest Events",
+                      align: TextAlign.center,
+                    ),
+                    SizedBox(
+                      height: kDefaultPadding,
+                    ),
+                    SubHeadingText(
+                      text:
+                          "Filter by sports, area, availability, and much more!",
+                      align: TextAlign.center,
+                    ),
+                    SizedBox(
+                      height: kDefaultPadding * 2.0,
+                    ),
+                    Container(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: null,
+                        child: Padding(
+                          padding: const EdgeInsets.all(kDefaultPadding),
+                          child: Text(
+                            "View Events",
+                            style: TextStyle(
+                              fontFamily: "Poppins",
+                              fontSize: 18,
+                              fontWeight: FontWeight.w700,
                               color: Colors.white,
                             ),
-                            onClick: () {
-                              print('clicked find location button');
-                            },
                           ),
                         ),
-                      ),
-                      Transform.translate(
-                        offset: Offset.fromDirection(getRadiansFromDegree(225),
-                            degOneTranslationAnimation.value * 100),
-                        child: Transform(
-                          transform: Matrix4.rotationZ(
-                              getRadiansFromDegree(rotationAnimation.value))
-                            ..scale(degOneTranslationAnimation.value),
-                          alignment: Alignment.center,
-                          child: CircularButton(
-                            color: Colors.blue,
-                            width: 50,
-                            height: 50,
-                            icon: Icon(
-                              Icons.filter_list,
-                              color: Colors.white,
-                            ),
-                            onClick: () {
-                              print('clicked filter button');
-                            },
-                          ),
+                        style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(secondaryColor),
+                          foregroundColor:
+                              MaterialStateProperty.all(secondaryColor),
+                          shape:
+                              MaterialStateProperty.all(RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(48),
+                          )),
                         ),
                       ),
-                      Transform.translate(
-                        offset: Offset.fromDirection(getRadiansFromDegree(180),
-                            degOneTranslationAnimation.value * 100),
-                        child: Transform(
-                          transform: Matrix4.rotationZ(
-                              getRadiansFromDegree(rotationAnimation.value))
-                            ..scale(degOneTranslationAnimation.value),
-                          alignment: Alignment.center,
-                          child: CircularButton(
-                            color: Colors.blue,
-                            width: 50,
-                            height: 50,
-                            icon: Icon(
-                              Icons.search,
-                              color: Colors.white,
-                            ),
-                            onClick: () {
-                              print('clicked search button');
-                            },
-                          ),
-                        ),
-                      ),
-                      Transform(
-                        transform: Matrix4.rotationZ(
-                            getRadiansFromDegree(rotationAnimation.value)),
-                        alignment: Alignment.center,
-                        child: CircularButton(
-                            color: primaryColor,
-                            width: 60,
-                            height: 60,
-                            icon: Icon(
-                              Icons.map,
-                              color: Colors.white,
-                            ),
-                            onClick: () {
-                              if (animationController.isCompleted) {
-                                animationController.reverse();
-                              } else {
-                                animationController.forward();
-                              }
-                            }),
-                      )
-                    ],
-                  ))
-            ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
         ),
       ],
     );
+    // return ScaffoldWithGradient(
+    //   menuColor: primaryColor,
+    //   children: <Widget>[
+    //     Container(
+    //       height: size.height * 0.70,
+    //       decoration: BoxDecoration(
+    //         borderRadius: BorderRadius.only(
+    //           bottomLeft: Radius.circular(10.0),
+    //           bottomRight: Radius.circular(10.0),
+    //         ),
+    //       ),
+    // child: GoogleMap(
+    //   myLocationButtonEnabled: false,
+    //   zoomControlsEnabled: false,
+    //   initialCameraPosition: CameraPosition(
+    //     target: LatLng(24.142, -110.321),
+    //     zoom: 15,
+    //   ),
+    // ),
+    //     ),
+    //   ],
+    // );
   }
 }
 
 class MapView extends StatefulWidget {
   State<StatefulWidget> createState() => MapState();
-}
-
-class CircularButton extends StatelessWidget {
-  final double width;
-  final double height;
-  final Color color;
-  final Icon icon;
-  final Function onClick;
-
-  CircularButton(
-      {this.color, this.width, this.height, this.icon, this.onClick});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(color: color, shape: BoxShape.circle),
-      width: width,
-      height: height,
-      child: IconButton(icon: icon, enableFeedback: true, onPressed: onClick),
-    );
-  }
 }
