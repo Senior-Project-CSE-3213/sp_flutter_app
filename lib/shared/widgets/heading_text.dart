54be40c5feffd49acee8a2894bf801f363620855
Justin Smith <justinasmith662@gmail.com>
2021-03-17 22:10:28 -0500

    Add align property

--- lib/shared/widgets/heading_text.dart
+++ lib/shared/widgets/heading_text.dart
@@ -4,14 +4,17 @@ class HeadingText extends StatelessWidget {
   const HeadingText({
     Key key,
     @required this.text,
+    this.align,
   }) : super(key: key);
 
   final String text;
+  final TextAlign align;
 
   @override
   Widget build(BuildContext context) {
     return Text(
       "$text",
+      textAlign: align ?? TextAlign.left,
       style: TextStyle(
         color: Colors.white,
         fontSize: 48,
