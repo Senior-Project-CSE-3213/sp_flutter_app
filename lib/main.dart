bb76322ab65746fcd3d93000813f5b6637b113a7
JustinASmith <justinasmith662@gmail.com>
2021-03-19 14:53:18 -0500

    Update main.dart
    Test commit message

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ import 'views/settings/settings.dart';
 import 'shared/constants.dart';
 import 'viewmodels/user_viewmodel.dart';
 
+//test
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
